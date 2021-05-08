e6db052e60a8dbadbe3cd9b61b4b46638989e9bc
dylanpruitt <dylan.pruitt.19@cnu.edu>
2021-05-08 08:11:15 -0400

    Added basic bullet functionality

---
+++ bullets.js
@@ -0,0 +1,41 @@
+let basicBullet = (x, y, speedX, speedY, creatorName) => {
+    let bullet = {
+        creatorName: creatorName,
+        damage: 3,
+        x: x,
+        y: y,
+        speedX: speedX,
+        speedY: speedY,
+        speedCap: 2,
+        width: 4,
+        height: 4,
+        image: new Image(),
+        framesActive: 0,
+        maxFramesActive: 60,
+        update: () => {
+            bullet.updatePosition();
+            bullet.updateSpeed();
+            bullet.framesActive++;
+        },
+        updatePosition: () => {
+            bullet.x += bullet.speedX;
+            bullet.y += bullet.speedY;
+        },
+        updateSpeed: () => {
+            if (bullet.speedX > 0) { bullet.speedX -= 0.05; }
+            if (bullet.speedY > 0) { bullet.speedY -= 0.05; }
+            if (bullet.speedX < 0) { bullet.speedX += 0.05; }
+            if (bullet.speedY < 0) { bullet.speedY += 0.05; }
+            if (Math.abs(bullet.speedX) < 0.1) { bullet.speedX = 0; }
+            if (Math.abs(bullet.speedY) < 0.1) { bullet.speedY = 0; }
+        },
+        onCollide: (entity) => {
+            if (entity.name !== bullet.creatorName) {
+                bullet.framesActive = bullet.maxFramesActive;
+                entity.health -= bullet.damage;
+            }
+        }
+    }
+    bullet.image.src = "bullet.png";
+    return bullet;
+}
